3e2e496ddbd641ee7c92a95c06b81d87eaadda1c
GSmurf <GSmurf@users.noreply.github.com>
2014-03-21 15:24:38 +0100

    Update robotDeplacement.ino
    découpage du fichier en plusieurs pour une meilleure compréhension

--- robotDeplacement/robotDeplacement.ino
+++ robotDeplacement/robotDeplacement.ino
@@ -36,12 +36,17 @@ const int DROITE     = 90;
 const int GAUCHE     = -90;
 const int DEMI_TOUR  = 180;
 
-// Constantes de distance
-const int TRES_PRES    = 0;
-const int PRES         = 1;
-const int LOIN         = 2;
-const int RIEN         = 3;
+// Constantes de distance le nombre exprime la distance en cm limite pour cette distance
+const int TRES_PRES    = 8;
+const int PRES         = 30;
+const int LOIN         = 70;
+const int RIEN         = 115;
 const int DISTANCE_MAX = 400; // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+
+const int STOP   = 0;
+const int LENT   = 100;
+const int MOYEN  = 150;
+const int RAPIDE = 255;
 
 // variable to store the servo position
 int pos = getAngleServo(DEVANT);     
@@ -57,40 +62,6 @@ Servo servo;  // create servo object to control a servo
 // Déclaration pour le compass
 HMC5883L mag;
 int16_t mx, my, mz;
-/* ------------------------------------------------------------------------
-* Code moteur
------------------------------------------------------------------------- */
-const int STOP   = 0;
-const int LENT   = 75;
-const int MOYEN  = 125;
-const int RAPIDE = 255;
-
-void moteurVitesse(int vitGauche, int vitDroite) {
-  analogWrite(PIN_MOTEUR1_PW, vitGauche);
-  analogWrite(PIN_MOTEUR2_PW, vitDroite);
-}
-
-void moteurEnAvant() {
-  digitalWrite(PIN_MOTEUR1_DIR,HIGH);
-  digitalWrite(PIN_MOTEUR2_DIR,HIGH);
-}
-
-void moteurEnArriere() {
-  digitalWrite(PIN_MOTEUR1_DIR,LOW);
-  digitalWrite(PIN_MOTEUR2_DIR,LOW);
-}
-
-// Permet de tourner vite à droite
-void moteurTourneADroite() {
-  digitalWrite(PIN_MOTEUR1_DIR,HIGH);
-  digitalWrite(PIN_MOTEUR2_DIR,HIGH);
-}
-
-// Permet de tourner vite à gauche
-void moteurTourneAGauche() {
-  digitalWrite(PIN_MOTEUR1_DIR,HIGH);
-  digitalWrite(PIN_MOTEUR2_DIR,HIGH);
-}
 /* ------------------------------------------------------------------------
 * Fonctions compass
 ------------------------------------------------------------------------ */
@@ -131,11 +102,11 @@ int detecteDistance() {
   // Lecture de la distance
   int x = litDistance();
   
-  if(x < 6) {
+  if(x < TRES_PRES) {
     return TRES_PRES;
-  } else if(x < 30) {
+  } else if(x < PRES) {
     return PRES;
-  } else if(x < 45) {
+  } else if(x < LOIN) {
     return LOIN;
   } else {
     return RIEN;
@@ -201,16 +172,16 @@ void tourneRobot(int direction){
   
   switch(direction) {
     case DROITE:
-      moteurTourneADroite();
-      moteurVitesse(MOYEN,STOP);
+      moteurEnAvant();
+      moteurVitesse(LENT,STOP);
       break;
     case GAUCHE:
-      moteurTourneAGauche();
-      moteurVitesse(STOP,MOYEN);
+      moteurEnAvant();
+      moteurVitesse(STOP,LENT);
       break;
     case DEMI_TOUR:
-      moteurTourneADroite();
-      moteurVitesse(MOYEN,MOYEN);
+      moteurEnAvant();
+      moteurVitesse(MOYEN,STOP);
       break;
   }
   // Calcule la postition d'arrivée
@@ -227,99 +198,6 @@ void tourneRobot(int direction){
   } while (!((positionActuelle > positionMin) && (positionActuelle < positionMax)));
   
   stop();
-}
-
-void debug(String texte){
-  Serial.print("*** ");Serial.print(texte);Serial.println(" ***");  
-  Serial.print("Droite :\t");Serial.print(distDroite);Serial.print("\tGauche :\t");Serial.print(distGauche);Serial.print("\tDevant :\t");Serial.println(distDevant);
-  Serial.print("Position :\t");Serial.println(pos);
-  Serial.println("---");
-}
-
-void debugRotation(float direction, float positionDebut, float positionActuelle, int positionMin, int positionMax){
-  Serial.print("*** ");Serial.print("DebugRotation , direction : \t");Serial.print(direction);Serial.println(" ***");  
-  Serial.print("Debut :\t");Serial.print(positionDebut);
-  Serial.print("\tActuelle :\t");Serial.print(positionActuelle);
-  Serial.print("\tMin :\t");Serial.print(positionMin);
-  Serial.print("\tMax :\t");Serial.println(positionMax);
-  Serial.println("---");
-}
-
-/* ------------------------------------------------------------------------
-* Fonctions IA
------------------------------------------------------------------------- */
-/*
-* Le robot avance à une certaine allure en fonction de la distance 
-* qu'il avait detecté devant lui dans la boucle loop
-* TODO : faire en sorte qu'il renvoi un boolean pour savoir s'il avance ou non
-*/
-void ia_avanceAllure(){
-  switch(distDevant) {
-    case TRES_PRES:
-      stop();
-      break;
-    case PRES:
-      moteurEnAvant();
-      moteurVitesse(LENT,LENT);
-      break;
-    case LOIN:
-      moteurEnAvant();
-      moteurVitesse(MOYEN,MOYEN);
-      break;
-    case RIEN:
-      moteurEnAvant();
-      moteurVitesse(RAPIDE,RAPIDE);
-      break;
-  }
-}
-
-/*
-* Scan la direction la plus courte pour y tourner le robot
-*/
-void ia_choixDirection(){
-  scanDirection();
-  // si la distance à droite et à gauche est courte alors
-  if((distDroite == TRES_PRES) && (distGauche == TRES_PRES)){
-    // fait demi tour
-    tourneRobot(DEMI_TOUR);
-  }else{
-    // si la distance à droite et la plus courte ou egale à gauche alors
-    if(distDroite <= distGauche){
-      // tourne à droite
-      tourneRobot(DROITE);
-    }else{
-      // tourne à gauche
-      tourneRobot(GAUCHE);
-    }
-  }
-}
-
-// Viens tater l'obstacle devant lui
-void ia_essayeDePasser(){}
-
-// Suis l'obstacle devant lui
-void ia_suisUneCible(){}
-
-// Va se ranger dans le coin cardinal demandé
-void ia_vaSeRangerDansLeCoin(){}
-
-// Detecte ou es la cible
-int ia_chercheAngleCible(int ancienneDistance){
-	int i;
-	float tolerance = 0.1;
-	int distMax = int(ancienneDistance + (ancienneDistance*tolerance));
-	int distMin = int(ancienneDistance - (ancienneDistance*tolerance));
-	int angle = 90;
-	int distance;
-	int angles[] = {0,36,72,108,144,180};
-	for (i = 0; i < 6; i = i + 1) {
-	  distance = regardeVers(angles[i]);
-	  if((distance <= distMax) && (distMax >= distMin)){
-	  angle = angles[i];
-	  }
-	}
-
-	return angle;
 }
 /* ------------------------------------------------------------------------
 * Lancement du programme
@@ -354,7 +232,5 @@ void loop(){
   }else{
     // sinon determine son allure en fonction de la distance à un potentiel obstacle
 	ia_avanceAllure();
-  } 
-      
-  if(DEBUG)delay(TEMPS_CYCLE);
+  }
 }
